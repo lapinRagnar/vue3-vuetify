9590e815a1b549e366f1d128f0b89447a89db440
lapinRagnar <lapinragnar@gmail.com>
2022-09-06 11:45:50 +0200

    supprimer le hello world

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
     <v-main>
+
+      
+
       <router-view/>
     </v-main>
   </v-app>
@@ -8,11 +11,13 @@
 
 <script>
 
-export default {
-  name: 'App',
 
-  data: () => ({
-    //
-  }),
+export default {
+    name: "App",
+    data: () => ({
+        //
+
+    }),
+    components: {  }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    home
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    
   },
 });
 </script>
